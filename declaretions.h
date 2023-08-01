6168ec4ea59e8ee8ef6d8b74d5b39281c1541cc0
HiShiGaming <111409592+HiShiGaming@users.noreply.github.com>
2023-08-01 17:14:26 +0700

    Update declaretions.h

--- declaretions.h
+++ declaretions.h
@@ -1,4 +1,3 @@
-//Thư Viện khai báo chân động cơ và setup
 #include <ESP32_Servo.h>
 #include "BluetoothSerial.h"
 // Khai báo chân động cơ
@@ -6,16 +5,18 @@ const int Motor1Pin1 = 12;
 const int Motor1Pin2 = 23;
 const int Motor2Pin1 = 33;
 const int Motor2Pin2 = 32;
-const int Motor3Pin1 = 4;
+const int Motor3Pin1 = 4 ;
 const int Motor3Pin2 = 16;
 // 
-int pwmValue = 0;
-String device_name = "Robot";
+int pwmValue         = 0;
+int MaxpwmValue      = 0;
+String device_name   = "Robot";
 BluetoothSerial SerialBT;
 Servo myservo;
-int pos = 0;
-int servoPin = 17;
-void setup() {
+int pos              = 0;
+int servoPin         = 17;
+void setup() 
+{
   // Khởi tạo các chân kết nối với động cơ là OUTPUT
   pinMode(Motor1Pin1, OUTPUT);
   pinMode(Motor1Pin2, OUTPUT);
